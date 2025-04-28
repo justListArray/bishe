6758204225b8366297221b0e9f43a4a50e7d4f95
Ubuntu <ubuntu@localhost.localdomain>
2025-04-28 21:26:02 +0800

    update

--- controllers/match/PerdictController.go
+++ controllers/match/PerdictController.go
@@ -4,8 +4,10 @@ import (
 	"encoding/json"
 	"fmt"
 	"footballsys/controllers/base"
+	"footballsys/models"
 	"io"
 	"net/http"
+	"net/url"
 	"strconv"
 
 	"github.com/gin-gonic/gin"
@@ -15,20 +17,8 @@ type PerdictController struct {
 	base.BaseController
 }
 
-func (perdict PerdictController) Prediction(c *gin.Context) {
-	// var perd string
-	// err := c.ShouldBindJSON(&perd)
-	// if err != nil {
-	// 	c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-	// 	return
-	// }
-	// c.JSON(http.StatusOK, gin.H{
-	// 	"message": perd,
-	// })
-	fixture := 198772 //处理？
-
-	// Get all available predictions from one {fixture}
-	//get("https://v3.football.api-sports.io/predictions?fixture=198772");  //test
+func (per PerdictController) Perdict(fixture int) gin.H {
+	fixture = 198772 //strReq队伍对应的id（fixture） ,我们通过team_id 查询team的比赛，如果想要预测，就调用Predict
 	url := "https://v3.football.api-sports.io/predictions?fixture=" + strconv.Itoa(fixture)
 	method := "GET"
 
@@ -37,7 +27,6 @@ func (perdict PerdictController) Prediction(c *gin.Context) {
 
 	if err != nil {
 		fmt.Println(err)
-		return
 	}
 	req.Header.Add("x-rapidapi-key", "aa8bc3a99aeedaedab0dfe40555e4386")
 	req.Header.Add("x-rapidapi-host", "v3.football.api-sports.io")
@@ -45,23 +34,167 @@ func (perdict PerdictController) Prediction(c *gin.Context) {
 	res, err := client.Do(req)
 	if err != nil {
 		fmt.Println(err)
-		return
 	}
 	defer res.Body.Close()
 
 	body, err := io.ReadAll(res.Body)
 	if err != nil {
 		fmt.Println(err)
-		return
 	}
-	//fmt.Println("Response Body:", string(body))
+	fmt.Println("Response Body:", string(body))
 	var response map[string]interface{} //  接收body
 	err = json.Unmarshal(body, &response)
 	if err != nil {
 		fmt.Println(err)
 	}
+
+	// c.JSON(http.StatusOK, gin.H{
+	// 	"message": response,
+	// })
+	var strPerdiction models.PResponse
+	responseData := response["response"].([]interface{})[0].(map[string]interface{})
+	strPerdiction.Predictions.Winner.ID = int(responseData["predictions"].(map[string]interface{})["winner"].(map[string]interface{})["id"].(float64))
+	strPerdiction.Predictions.Winner.Name = responseData["predictions"].(map[string]interface{})["winner"].(map[string]interface{})["name"].(string)
+	strPerdiction.Predictions.Winner.Comment = responseData["predictions"].(map[string]interface{})["winner"].(map[string]interface{})["comment"].(string)
+
+	strPerdiction.League.ID = int(responseData["league"].(map[string]interface{})["id"].(float64))
+	strPerdiction.League.Name = responseData["league"].(map[string]interface{})["name"].(string)
+	strPerdiction.League.Logo = responseData["league"].(map[string]interface{})["logo"].(string)
+
+	strPerdiction.Teams.Home.ID = int(responseData["teams"].(map[string]interface{})["home"].(map[string]interface{})["id"].(float64))
+	strPerdiction.Teams.Home.Name = responseData["teams"].(map[string]interface{})["home"].(map[string]interface{})["name"].(string)
+	strPerdiction.Teams.Home.Logo = responseData["teams"].(map[string]interface{})["home"].(map[string]interface{})["logo"].(string)
+
+	strPerdiction.Teams.Away.ID = int(responseData["teams"].(map[string]interface{})["away"].(map[string]interface{})["id"].(float64))
+	strPerdiction.Teams.Away.Name = responseData["teams"].(map[string]interface{})["away"].(map[string]interface{})["name"].(string)
+	strPerdiction.Teams.Away.Logo = responseData["teams"].(map[string]interface{})["away"].(map[string]interface{})["logo"].(string)
+
+	keyInfo := gin.H{
+		"Winner": strPerdiction.Predictions.Winner,
+		"League": strPerdiction.League,
+		"Teams": gin.H{
+			"Home": strPerdiction.Teams.Home,
+			"Away": strPerdiction.Teams.Away,
+		},
+	}
+	return keyInfo
+}
+
+func QueryTeamID(name string, c *gin.Context) (fixture int) { //通过teamname找到teamid
+	url := "https://v3.football.api-sports.io/teams?name=" + name
+	method := "GET"
+
+	client := &http.Client{}
+	req, err := http.NewRequest(method, url, nil)
+	if err != nil {
+		fmt.Println(err)
+	}
+	req.Header.Add("x-rapidapi-key", "aa8bc3a99aeedaedab0dfe40555e4386")
+	req.Header.Add("x-rapidapi-host", "v3.football.api-sports.io")
+
+	res, err := client.Do(req)
+	if err != nil {
+		fmt.Println(err)
+	}
+	body, err := io.ReadAll(res.Body)
+	if err != nil {
+		fmt.Println(err)
+	}
+	// fmt.Println("Response Body:", string(body))
+	var response map[string]interface{}
+	err = json.Unmarshal(body, &response)
+	if err != nil {
+		fmt.Println(err)
+	}
+	// fmt.Println(response)
+	// c.JSON(http.StatusOK, gin.H{
+	// 	"message": response,
+	// })
+	responseData := response["response"].([]interface{})[0].(map[string]interface{})
+	team := responseData["team"].(map[string]interface{})
+	teamId := team["id"].(float64)
+	// c.JSON(http.StatusOK, gin.H{
+	// 	"MESSAGE": teamId,
+	// })
+	return int(teamId) //id
+}
+
+// 通过teamid找到下一次的fixtureid
+func QueryTeamNextFixture(teamid int, c *gin.Context) (fixture int) {
+	teamID := teamid
+	season := "2024"
+	baseURL := "https://v3.football.api-sports.io/fixtures" //换成下场比赛的api  ?
+	params := url.Values{}
+	params.Add("team", strconv.Itoa(teamID))
+	params.Add("season", season)
+	url1 := baseURL + "?" + params.Encode()
+	method := "GET"
+
+	req, err := http.NewRequest(method, url1, nil)
+	if err != nil {
+		fmt.Println(err)
+	}
+	req.Header.Add("x-rapidapi-key", "aa8bc3a99aeedaedab0dfe40555e4386")
+	req.Header.Add("x-rapidapi-host", "v3.football.api-sports.io")
+
+	client := &http.Client{}
+	res, err := client.Do(req)
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer res.Body.Close()
+
+	body, err := io.ReadAll(res.Body)
+	if err != nil {
+		fmt.Println(err)
+	}
+	// fmt.Println("Response Body:", string(body))
+	var response map[string]interface{}
+	err = json.Unmarshal(body, &response)
+	if err != nil {
+		fmt.Println(err)
+	}
+	if errors, ok := response["errors"].(map[string]interface{}); ok {
+		if seasonError, ok := errors["season"].(string); ok {
+			fmt.Println("Error:", seasonError)
+			return
+		}
+	}
+	responseData := response["response"].([]interface{})
+
+	if len(response) == 0 {
+		fmt.Println("No fixtures found for the specified team and season.")
+		return
+	}
+
+	// 打印接下来几场比赛的信息
+	for _, fixtureData := range responseData {
+		fixture := fixtureData.(map[string]interface{})
+		fixtureID := int(fixture["fixture"].(map[string]interface{})["id"].(float64))
+		homeTeam := fixture["teams"].(map[string]interface{})["home"].(map[string]interface{})["name"].(string)
+		awayTeam := fixture["teams"].(map[string]interface{})["away"].(map[string]interface{})["name"].(string)
+
+		fmt.Printf("Fixture ID: %d\n", fixtureID)
+		fmt.Printf("Home Team: %s\n", homeTeam)
+		fmt.Printf("Away Team: %s\n", awayTeam)
+		fmt.Println("--------------------")
+	}
 	c.JSON(http.StatusOK, gin.H{
-		"message": response,
+		"message": responseData,
 	})
 
+	return fixture
+}
+func (per PerdictController) Prediction(c *gin.Context) {
+	//完成我们通过team_id 查询team的比赛，如果想要预测，就调用Predict
+	//下一场：
+	name := c.Query("name")
+	CopyFixture := QueryTeamID(name, c)
+	fixture := QueryTeamNextFixture(CopyFixture, c)
+	fmt.Println(fixture)
+	fmt.Println(CopyFixture)
+	//keyInfo := PerdictController.Perdict(per, fixture)
+	// c.JSON(http.StatusOK, gin.H{
+	// 	"message": keyInfo,
+	// })
 }
